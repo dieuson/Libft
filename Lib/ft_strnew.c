cac30cda7a2580cb5d35715919c935f58f53c8b5
Dieuson VIRGILE <dvirgile@e3r5p11.42.fr>
2015-12-24 10:21:00 +0100

    libft

---
+++ Lib/ft_strnew.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dvirgile <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/27 15:38:05 by dvirgile          #+#    #+#             */
+/*   Updated: 2015/12/02 14:15:14 by dvirgile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char				*ft_strnew(size_t size)
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+	while (i < size)
+	{
+		str[i] = '\0';
+		i++;
+	}
+	return (str);
+}
